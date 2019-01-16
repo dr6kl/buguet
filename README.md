ed0fcf55cca52bc6d1d618d0665f7fb5eafa04f5
dr6kl <dr6kl@protonmail.com>
2019-01-16 19:04:00 +0300

    Make a package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Buguet
+
+TODO
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="buguet",
+    version="0.0.1",
+    author="dr6kl",
+    author_email="dr6kl@protonmail.com",
+    description="Ethereum debugger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dr6kl/buguet",
+    packages=setuptools.find_packages(),
+    license = "GPL",
+    install_requires=['web3 >=4.7.2, <5', 'pysha3 >=1.0.2, <2', 'termcolor >=1.1.0, <2'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
+        "Operating System :: OS Independent",
+    ],
+)
